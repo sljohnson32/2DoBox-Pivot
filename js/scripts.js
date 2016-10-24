2ab52484bf7e73b3c83df45a18b78750b7c00a6f
sljohnson32 <sljohnson32@gmail.com>
2016-10-24 15:54:41 -0600

    add storage function for idea object

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
+var ideaCount = 100;
 var $title = $('#title-input');
 var $body = $('#body-input');
 
 function NewIdea() {
-  this.title = $title.value;
-  this.body = $body.value;
+  this.id = 'UID' + ideaCount;
+  this.title = $title.val();
+  this.body = $body.val();
+  this.quality = 3;
   /*jshint multistr: true */
   this.html = "<article class='idea-box'>\
       <h2>" + this.title + "</h2>\
@@ -12,10 +15,12 @@ function NewIdea() {
       <button type='button' name='button' id='up-button'></button>\
       <button type='button' name='button' id='down-button'></button>\
     </article>";
+  ideaCount++;
 }
 
 $('#save-button').on('click', function() {
-    console.log("Save button is working");
     var newIdeaBox = new NewIdea();
+    var storageId = newIdeaBox.id;
+    localStorage.setItem(storageId, JSON.stringify(newIdeaBox));
     $('.idea-container').append(newIdeaBox.html);
   });
