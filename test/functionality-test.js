1919213e48bca6ef0359b289221d5100d4eb30ac
Elijah Williams <ejwill04@gmail.com>
2016-12-04 14:13:22 -0700

    WIP-feature testing

--- test/functionality-test.js
+++ test/functionality-test.js
@@ -11,7 +11,7 @@ describe('new functionality bundle', function () {
 
 test.describe('testing ideabox', function() {
   this.timeout(10000);
-  test.it('should allow me to add a title and a description', ()=>{
+  test.it('should allow me to add a title and a task', ()=>{
     const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
@@ -19,19 +19,88 @@ test.describe('testing ideabox', function() {
     driver.get('http://localhost:8080');
 
     const title = driver.findElement({name: 'title'});
-    const description = driver.findElement({name: 'description'});
+    const task = driver.findElement({name: 'task'});
+
     title.sendKeys('this is a title').then(()=>{
       return title.getAttribute('value');
     }).then((value)=>{
       assert.equal(value, 'this is a title');
     });
 
-    description.sendKeys('this is a description').then(()=>{
-      return description.getAttribute('value');
+    task.sendKeys('this is a task').then(()=>{
+      return task.getAttribute('value');
     }).then((value)=>{
-      assert.equal(value, 'this is a description');
+      assert.equal(value, 'this is a task');
     });
 
     driver.quit();
   });
+
+  test.it('should allow me to submit a title and a task', ()=>{
+    const driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+    driver.get('http://localhost:8080');
+
+    const title = driver.findElement({name: 'title'});
+    const task = driver.findElement({name: 'task'});
+    const save = driver.findElement({name: 'button'});
+
+    title.sendKeys('this is a title');
+    task.sendKeys('this is a task');
+    save.click();
+
+    // driver.findElement({className: ‘delete-idea’}).click()
+    assert.equal(className: 'td-title', 'this is a title');
+
+    // title.sendKeys('this is a title').then( function() {
+    //   return title.getAttribute('value');
+    // }).then(function(value){
+    //   assert.equal(value, 'this is a title');
+    // });
+    //
+    // task.sendKeys('this is a task').then( function() {
+    //   return task.getAttribute('value');
+    // }).then(function(value){
+    //   assert.equal(value, 'this is a task');
+    // });
+
+    driver.quit();
+  });
+
+  // test.it('can add multiple ideas', ()=>{
+  //   this.timeout(10000);
+  //   const driver = new webdriver.Builder()
+  //   .forBrowser('chrome')
+  //   .build();
+  //
+  //   driver.get('http://localhost:8080');
+  //
+  //   const title = driver.findElement({name:'title'});
+  //   title.sendKeys('this is a title');
+  //
+  //   const task = driver.findElement({name:'task'});
+  //   task.sendKeys('this is a task');
+  //
+  //   const submit = driver.findElement({name: 'button'});
+  //   submit.click();
+  //
+  //   title.sendKeys('this is a title 2');
+  //
+  //   task.sendKeys('this is a task 2');
+  //
+  //   submit.click();
+  //
+  //   var allIdeas = driver.findElements({tagName: 'article'});
+  //   driver.findElements({tagName:'article'}).then((article) =>{
+  //     assert.equal(article.length, 2);
+  //   });
+  //
+  //
+  //   driver.findElements({tagName: 'article'}).then((article)=>{
+  //     assert.equal(article.length, 1);
+  //   });
+  //   driver.quit();
+  // });
 });
